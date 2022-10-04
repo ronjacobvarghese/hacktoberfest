866fca51396fb85f6e83405f897e7cd370a9f35f
Adithya Nair <adityanair999@gmail.com>
2022-10-05 05:13:52 +0530

    Diffof2No.py
    find the difference between 2 numbers in python.
    DOne as part of HacktoberFest

---
+++ Diff2No.py
@@ -0,0 +1,8 @@
+def diff(n1,n2):
+	if n1 > n2:
+		print(f"Difference of {n1} and {n2} is {n1-n2}")
+	else:
+		print(f"Difference of {n2} and {n1} is {n2-n1}")
+
+num1 = int(input())
+num2 = int(input())
